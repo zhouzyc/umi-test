87660abdcc3ada2cd0c8c9b9d305da828c53bad6
zhouyingchao <1212>
2020-11-12 17:09:09 +0800

    提交错误说明

---
+++ src/pages/docs/index.md
@@ -0,0 +1,11 @@
+---
+title: docs/组件使用实例
+group:
+  title: pages
+---
+
+<code src="./index.tsx" />
+
+
+<API></API>
+
--- src/pages/docs/index.tsx
+++ src/pages/docs/index.tsx
@@ -1,12 +1,78 @@
-import React from 'react';
-export interface IHelloProps {
+/**
+ * title: 组件使用实例
+ * desc: 我是简介，我可以用 `Markdown` 来编写
+ *
+ *
+ * motions:
+ *  - click:[data-action="addon"]
+ *  - timeout:1000
+ *  - click:[data-action="addon"]
+ *  - timeout:1000
+ *  - click:[data-action="reset"]
+ */
+import React, { useState } from 'react';
+
+/**
+ * demo properties.
+ */
+export interface IDemoProps {
   /**
-   * 可以这样写属性描述
-   * @description       也可以显式加上描述名
-   * @description.zh-CN 还支持不同的 locale 后缀来实现多语言描述
-   * @default           支持定义默认值
+   * 用户名
    */
-  className?: string; // 支持识别 TypeScript 可选类型为非必选属性
+  name: string;
+  /**
+   * 年龄
+   */
+  age?: number;
+  /**
+   * 工作
+   * @default doctor
+   */
+  work?: string;
+  /**
+   * 修改名字
+   * @param name
+   */
+  changeName?: (name: string) => void;
+
+  type: 'submit' | 'button';
+
+  /**
+   * 尺寸
+   * @deprecated 使用 size2 替代
+   */
+  size?: 'small' | 'default';
+
+  /**
+   * 新的尺寸
+   * @since 1.1.0
+   * @default large
+   */
+  size2?: 'small' | 'large';
 }
-const Hello: React.FC<IHelloProps> = () => <>Hello World!</>;
-export default Hello;
+
+function demo(props: IDemoProps) {
+  const [count, setCount] = useState(0);
+
+  /**
+   * 点击事件
+   * @public
+   */
+  const handledClick = (e: any) => {
+    setCount(count + 1);
+  };
+
+  return (
+    <>
+      <p>{count}</p>
+      <button type="button" data-action="addon" onClick={handledClick}>
+        增加
+      </button>
+      <button type="button" data-action="reset" onClick={() => setCount(0)}>
+        重置
+      </button>
+    </>
+  );
+}
+
+export default demo;
